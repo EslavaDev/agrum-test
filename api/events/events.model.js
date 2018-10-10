838075ffcfb52fa0a36a3d1e244f31ca321bb700
EslavaDev <danieleslava52@gmail.com>
2018-10-09 21:15:13 -0500

    id primary key

--- api/events/events.model.js
+++ api/events/events.model.js
@@ -3,6 +3,11 @@ const Schema = mongoose.Schema;
 
 const eventsSocketSchema = Schema({
   workgroup: String,
-}, { strict: false });
+  id: {
+    type: ObjectId,
+    index: true,
+    unique: true
+  },
+}, { strict: false,  versionKey: false, _id: false, id:true });
 
 module.exports= mongoose.model('eventsSocket', eventsSocketSchema);
